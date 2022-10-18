dcf9f404934f7cefd93168f070aa847b0bac11a6
Mickie <mickystn10@gmail.com>
2022-10-18 19:15:38 +0700

    lab8
    lab8

--- lab8/lab8_3.php
+++ lab8/lab8_3.php
@@ -1,13 +1,13 @@
 <?php
-include_once "connect.php";
-$stmt = $pdo->prepare("SELECT * FROM `member`");
-$stmt->execute();
-while ($row = $stmt->fetch()) {
-echo "ชื่อ  : " . $row ["name"] . "<br>";
-echo "ที่อยู่  : " . $row ["address"] . "<br>";
-echo "อีเมล  : " . $row ["email"] . "<br>";
-$img = "../member_photo/".$row["username"].".jpg";
-echo  "<img src='$img' />";
-echo "<hr>\n";
-}
+    include_once "connect.php";
+    $stmt = $pdo->prepare("SELECT * FROM `member`");
+    $stmt->execute();
+    while ($row = $stmt->fetch()) {
+        echo "ชื่อ  : " . $row ["name"] . "<br>";
+        echo "ที่อยู่  : " . $row ["address"] . "<br>";
+        echo "อีเมล  : " . $row ["email"] . "<br>";
+        $img = "../member_photo/".$row["username"].".jpg";
+        echo  "<img src='$img' />";
+        echo "<hr>\n";
+    }
 ?>
